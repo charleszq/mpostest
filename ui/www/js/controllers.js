540fb18c1ba3d51622d8c92203a6a81321be34e8
Qiang Zhang <qia.zhang@sap.com>
2015-06-12 10:10:13 +0800

    change to a blank ui app
    Change-Id: Ie648ba63e6724b4f76eb88c33e7a10d97bf5f4c4

---
+++ ui/www/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('starter.controllers', [])
+	.controller('MainCtrl', function($scope, $ionicModal, $timeout) {
+		$scope.setupDevice = function() {
+			var clientdata = {
+				billsMID: '898000156911002',
+				billsTID:  '00019130'
+			};
+			Mpos.setupDevice(clientdata,function(msg) {
+				alert(msg);
+			}, function() {
+				alert('error');
+			});
+		}
+	});
