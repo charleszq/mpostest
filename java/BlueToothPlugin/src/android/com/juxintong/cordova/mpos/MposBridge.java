1f0b5f21741c14b6781ddb06ca471b38291a192a
Qiang Zhang <qia.zhang@sap.com>
2015-06-17 16:07:06 +0800

    wait for service inited.
    Change-Id: Iedef0e60c710ddb8f88e4a376cc6b121e7cd602c

--- java/BlueToothPlugin/src/android/com/juxintong/cordova/mpos/MposBridge.java
+++ java/BlueToothPlugin/src/android/com/juxintong/cordova/mpos/MposBridge.java
@@ -25,31 +25,41 @@ import com.chinaums.mpos.service.IUmsMposService;
  * @author Charles
  * 
  */
-public class MposBridge extends CordovaPlugin {
+public class MposBridge extends CordovaPlugin implements ServiceConnection {
 
 	private static final String TAG = MposBridge.class.getSimpleName();
 	private IUmsMposService mUmsMposService;
-	private ServiceConnection mConnection = new ServiceConnection() {
-		@Override
-		public void onServiceConnected(ComponentName name, IBinder service) {
-			mUmsMposService = IUmsMposService.Stub.asInterface(service);
-		}
-
-		@Override
-		public void onServiceDisconnected(ComponentName name) {
-			mUmsMposService = null;
-		}
-	};
 
 	@Override
-	public boolean execute(String action, JSONArray data, final CallbackContext cb)
-			throws JSONException {
-		if( mUmsMposService == null ) {
-				initService();
+	public boolean execute(final String action, final JSONArray data,
+			final CallbackContext cb) throws JSONException {
+		if (mUmsMposService == null) {
+			initService(new ServiceConnection() {
+
+				@Override
+				public void onServiceConnected(ComponentName arg0,
+						IBinder service) {
+					mUmsMposService = IUmsMposService.Stub.asInterface(service);
+					IActionHandler handler = ActionHandlerFactory.getInstance()
+							.getActionHandler(mUmsMposService, action);
+					try {
+						handler.handle(data, cb);
+					} catch (JSONException e) {
+					}
+				}
+
+				@Override
+				public void onServiceDisconnected(ComponentName arg0) {
+					mUmsMposService = null;
+				}
+
+			});
+			return true;
 		}
-		
-		IActionHandler handler = ActionHandlerFactory.getInstance().getActionHandler(mUmsMposService, action);
-		if( handler == null ) {
+
+		IActionHandler handler = ActionHandlerFactory.getInstance()
+				.getActionHandler(mUmsMposService, action);
+		if (handler == null) {
 			return false;
 		} else {
 			return handler.handle(data, cb);
@@ -60,10 +70,10 @@ public class MposBridge extends CordovaPlugin {
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 		super.initialize(cordova, webView);
 		Log.d(TAG, "In initialize.");
-		initService();		
+		initService(this);
 	}
-	
-	private void initService() {
+
+	private void initService(ServiceConnection connection) {
 		Context context = cordova.getActivity();
 		Intent intent = new Intent();
 		SharedPreferences mSharedPreferences = context.getSharedPreferences(
@@ -76,14 +86,23 @@ public class MposBridge extends CordovaPlugin {
 		intent.setClassName(servicePkgname,
 				"com.chinaums.mpos.service.MposService");
 
-		context.bindService(intent, mConnection, Service.BIND_AUTO_CREATE);
+		context.bindService(intent, connection, Service.BIND_AUTO_CREATE);
 	}
 
 	@Override
 	public void onDestroy() {
 		Log.d(TAG, "Destroying...");
-		if (mConnection != null)
-			cordova.getActivity().unbindService(mConnection);
+		cordova.getActivity().unbindService(this);
 		super.onDestroy();
+	}
+
+	@Override
+	public void onServiceConnected(ComponentName arg0, IBinder service) {
+		mUmsMposService = IUmsMposService.Stub.asInterface(service);
+	}
+
+	@Override
+	public void onServiceDisconnected(ComponentName arg0) {
+		mUmsMposService = null;
 	}
 }
